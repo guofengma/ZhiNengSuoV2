805db2a44cd921c3adcf5402b5ee88e36682b30e
zhuyuesen <zhuyuesen>
2018-05-09 00:22:12 +0800

    一些测试

--- pages/index/index.js
+++ pages/index/index.js
@@ -49,9 +49,51 @@ Page({
       url: link,
     })
   },
+
+  //初始化小程序蓝牙模块
+  initBlue:function(e){
+    let that = this;
+    let promise = new Promise(function(resolve,jeject){
+      wx.openBluetoothAdapter({
+        success: function (res) {
+          console.log("success res -->", res);
+          resolve(res.errMsg);
+        },
+        fail: function (res) {
+          console.log("fail res -->", res);
+          jeject(res)
+        },
+      })
+    })
+    return promise;
+  },
+
+  // 获取本机蓝牙适配器状态
+  getBluetoothAdapterState:function(){
+    wx.getBluetoothAdapterState({
+      success: function (res) {
+        console.log("获取本机蓝牙适配器状态 suc->",res)
+      },
+      fail: function (res) {
+        console.log("获取本机蓝牙适配器状态fail -->", res);
+        jeject(res)
+      },
+    })
+  },
+
+  //初始化成功的回调
+  initSuccess:function(msg){
+    console.log("成功的回调",msg)
+  },
+
+  //初始化失败的回调
+  initErr:function(msg){
+    console.log("失败啦", msg)
+  },
  
   onLoad: function () {
-   
+    let that = this;
+    that.initBlue().then(that.getBluetoothAdapterState).catch(that.initErr)
   },
   
 })
